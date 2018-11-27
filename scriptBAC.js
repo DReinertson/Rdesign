19530c426ff86ebe7578cfdae53bf7463a5b4aca
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-11-27 08:40:26 -0800

    Add files via upload

--- scriptBAC.js
+++ scriptBAC.js
@@ -1,13 +1,27 @@
-$(document.body).on('click', function(){
-  console.log('click, moving items now');
-  $('.navItem').addClass('slideIn', 1000);
-  $('.about, .other').addClass('left', 2000);
-  $('.services, .contact').addClass('center', 2000);
-  $('.projects').addClass('right', 2000);
-})
+$('.navBar').hide();
 
-$('.navItem').on('click', function(e){
-  e.stopPropagation();
-  $('.services, .contact').removeClass('center', 1000);
-  $('.projects').removeClass('right', 1000);
+
+$(document).ready(function(){
+  $('.navItem').animate({
+    width: '50%'
+  }, 1000);
+  //$('.services').animate({left: '25%'}, 1500);
+  //$('.projects').animate({left: '50%'}, 1500);
+  //$('.contact').animate({left: '25%'}, 1500);
+});
+
+
+$('.navItem').on('click', function(){
+  let id = "#" + this.id
+  console.log(id);
+  $('.businessName').hide(500);
+  $('.navBar').show(1500);
+  $('.navItem').animate({left : '0'}, 1500);
+  $('.navItem span').animate({'margin-left': '0'}, {duration: 1000, queue:false});
+  $('.navItem').animate({top: '6%'}, {duration:1500, queue:false, complete:function(){
+    console.log('hidding all divs except', id);
+    $('.navItem').hide();
+    $(id).show();
+  }});
+
 })
