ca794806d8eadad496ab49320c599b5cf8717eb9
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2018-12-20 10:17:43 -0800

    Add files via upload
    Before adding multiple pages in the content container that will use hide/show() to show content based upon button click

--- leftNav/leftNav.js
+++ leftNav/leftNav.js
@@ -1,51 +1,55 @@
-let moved = false;
+$('.navMax').hide();
+
+function highlightActive(div){
+  console.log('div: ', div);
+  console.log(document.title);
+  $("#" + div).addClass('active');
+}
+let title = document.title.toLowerCase();
+highlightActive(title);
+
 
 //page will be a string, id, div, identifier.
-function showPage(page){
-  let url = './' + page + 'Page.html';
-  if (moved === false){
-    console.log(url);
-    $('.page').load(url).hide(250).fadeIn({duration: 250});
-    $('.page').addClass('pageSpecs');
-  }
-  else{
-    console.log('else statement showPage');
-    $('.page').load(url).hide().delay(250).fadeIn({duration: 250});
-  }
-};
+// function showPage(page){
+//   let url = './' + page + 'Page.html';
+//   if (moved === false){
+//     console.log(url);
+//     $('.page').load(url).hide(250).fadeIn({duration: 250});
+//     $('.page').addClass('pageSpecs');
+//   }
+//   else{
+//     console.log('else statement showPage');
+//     $('.page').load(url).hide().delay(250).fadeIn({duration: 250});
+//   }
+// };
 
-function moveNav(div){
-  if (moved === false){
-    console.log(div.id);
-    let id = '#' + div.id;
-    console.log(id);
-    $('.navItem').animate({fontSize: '2vw', height: '5%', 'max-width':'100%', margin:'0', paddingRight:'4%'}, 500);
+// function moveNav(div){
+//   if (moved === false){
+//     console.log(div.id);
+//     let id = '#' + div.id;
+//     console.log(id);
+//     $('.navItem').animate({fontSize: '2vw', height: '5%', 'max-width':'100%', margin:'0', paddingRight:'4%'}, 500);
+//
+//     $('.nav').animate({height: '100%', backgroundColor:'#808080', width: '12%'}, {duration: 500, complete:function(){
+//       console.log('going into animate function');
+//       $('.nav').addClass('fixed');
+//       //$('.businessName').animate({width:'88%'});
+//     }});
+//     showPage(div.id);
+//     moved = true;
+//   }
+//   else{
+//     console.log('else statement moveNav');
+//     showPage(div.id);
+//   }
+// }
 
-    $('.nav').animate({height: '100%', backgroundColor:'#808080', width: '12%'}, {duration: 500, complete:function(){
-      console.log('going into animate function');
-      $('.nav').addClass('fixed');
-      //$('.businessName').animate({width:'88%'});
-    }});
-    showPage(div.id);
-    moved = true;
-  }
-  else{
-    console.log('else statement moveNav');
-    showPage(div.id);
-  }
-}
-function highlightActive(div){
-  $('.navItem').each(function(){
-    $(this).removeClass('active');
-  });
-  $(div).addClass('active');
-}
 
-$('.navItem').on('click', function(){
-    moveNav(this);
-    highlightActive(this);
-
-});
+// $('.navItem').on('click mouseover', function(){
+//     moveNav(this);
+//     highlightActive(this);
+//
+// });
 
 
 
