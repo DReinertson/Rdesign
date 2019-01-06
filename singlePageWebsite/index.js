0049a1f7f184ae09145e01c67718d12e71f94ca2
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2019-01-05 21:02:53 -0800

    Add files via upload

---
+++ singlePageWebsite/index.js
@@ -0,0 +1,38 @@
+
+
+let open = false;
+function openNav(){
+  $('.nav').css({
+    height: '5vh'
+  });
+  open = true;
+}
+
+function closeNav(){
+  $('.nav').css({
+    height: 0
+  });
+  open = false;
+}
+
+$('i').on('click', function(){
+  if (open === false){
+    openNav();
+  }
+  else {
+    closeNav();
+  }
+  //$('.nav').toggle('slow', function(){
+    //$('.nav').animate({
+      //height: '5vh'
+      //})
+  //});
+});
+
+$(window).scroll(function(){
+  closeNav();
+});
+
+$(document).ready(function(){
+  openNav();
+})
