2760327b9b1b1c39023faa1ac8373fb696499473
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2019-01-09 22:56:07 -0800

    Add files via upload
    Before going too crazy with mobile queries

--- singlePageWebsite/index.js
+++ singlePageWebsite/index.js
@@ -1,23 +1,33 @@
-
 let pos = $(window).scrollTop();
-
 let open = false;
 function underline(id){
   $(id).addClass('underline');
 };
 function openNav(){
-  $('.nav').css({
-    height: '5vh'
-  });
-  open = true;
+  if($(window).innerWidth() > 992){
+    $('.nav').css({
+      height: '5vh'
+    });
+    open = true;
+  }
+  else{
+    $('.nav').addClass('fullscreen');
+    open = true;
+  }
 };
 
 function closeNav(){
-  $('.nav').css({
-    height: 0
-  });
-  open = false;
-};
+  if($(window).innerWidth() > 992){
+    $('.nav').css({
+      height: 0
+    });
+    open = false;
+  }
+  else{
+    $('.nav').removeClass('fullscreen');
+    open = false;
+  };
+}
 
 $('i').on('click', function(){
   if (open === false){
@@ -28,24 +38,21 @@ $('i').on('click', function(){
   }
 });
 
-
-
-$(window).scroll(function(){
-  let scroll = $(window).scrollTop();
-  if (scroll > pos){
-    console.log('Scroll Down');
-    closeNav();
-  }
-  else{
-    console.log('Scroll Up');
+if($(window).innerWidth() > 992){
+  console.log('large screen');
+  $(window).scroll(function(){
+    let scroll = $(window).scrollTop();
+    if (scroll > pos){
+      console.log('Scroll Down');
+      closeNav();
+    }
+    else{
+      console.log('Scroll Up');
+      openNav();
+    }
+    pos = scroll;
+  });
+  $(document).ready(function(){
     openNav();
-  }
-  pos = scroll;
-});
-
-
-
-$(document).ready(function(){
-  openNav();
-})
-
+  })
+}
