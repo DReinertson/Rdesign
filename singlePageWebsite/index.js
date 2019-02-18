489143bd7688cb78809ceafdba513667f4fddf49
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2019-02-18 13:55:12 -0800

    Add files via upload

--- singlePageWebsite/index.js
+++ singlePageWebsite/index.js
@@ -1,23 +1,45 @@
-
 let pos = $(window).scrollTop();
-
 let open = false;
+let page;
+
+
 function underline(id){
   $(id).addClass('underline');
 };
 function openNav(){
-  $('.nav').css({
-    height: '5vh'
-  });
-  open = true;
+  console.log('opening nav');
+  if($(window).width() > 1024){
+    $('.nav').removeClass('fullscreen').addClass('navHeight');
+    open = true;
+  }
+  else{
+    $('.nav').removeClass('navHeight').addClass('fullscreen');
+    open = true;
+  }
 };
 
 function closeNav(){
-  $('.nav').css({
-    height: 0
-  });
-  open = false;
-};
+  console.log('closing nav');
+  if($(window).width() > 1024){
+    $('.nav').removeClass('navHeight');
+    $('.nav').removeClass('fullscreen');
+    open = false;
+  }
+  else{
+    $('.nav').removeClass('fullscreen navHeight');
+    open = false;
+  };
+}
+function openPage(id){
+  if (page === id){
+    return;
+  }
+  $('.innerPage').fadeOut(100);
+  $('.' + id +'Page').delay(100).fadeIn(100);
+  page = id;
+}
+
+
 
 $('i').on('click', function(){
   if (open === false){
@@ -28,24 +50,31 @@ $('i').on('click', function(){
   }
 });
 
-
-
-$(window).scroll(function(){
-  let scroll = $(window).scrollTop();
-  if (scroll > pos){
-    console.log('Scroll Down');
-    closeNav();
-  }
+$(document).ready(function(){
+  if($(window).width() > 1024){
+      console.log('large screen, hiding i opening nav');
+      $('i').hide();
+      openNav();
+      $('.caption').hide();
+    }
   else{
-    console.log('Scroll Up');
-    openNav();
-  }
-  pos = scroll;
+      console.log('smaller screen, showing caption, hiding overlay');
+      $('.caption').show();
+      $('.overlay').hide();
+    }
 });
 
-
-
-$(document).ready(function(){
-  openNav();
-})
-
+$(window).scroll(function(){
+  if($(window).width() > 1024){
+    let scroll = $(window).scrollTop();
+    if (scroll > pos){
+      console.log('Scroll Down');
+      closeNav();
+    }
+    else{
+      console.log('Scroll Up');
+      openNav();
+    }
+    pos = scroll;
+  };
+});
