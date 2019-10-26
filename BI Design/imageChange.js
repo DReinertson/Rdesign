e9f3044f879c31e4f632b8d6b35700139edd5860
Darryn J. Reinertson <35150825+darryn11@users.noreply.github.com>
2019-10-26 09:01:52 -0700

    Before MAH restructure
    Moving pictures around to accommodate Client's description / organization of products

--- BI Design/imageChange.js
+++ BI Design/imageChange.js
@@ -25,43 +25,43 @@ $(document).ready(function(){
 let options = [
   {
     id : 'circleWhite',
-    src : './images/MAH/MAHCircle1.jpg',
+    src : './images/products/twelveRound/twelveRoundWhite.jpg',
     caption : 'Here is the caption for desk image',
     color: 'White'
   },
   {
     id : 'circleDarkGray',
-    src : './images/MAH/MAHCircle2.jpg',
+    src : './images/products/twelveRound/twelveRoundMetal.jpg',
     caption : 'Here is the caption for image 2',
-    color: "Dark Gray"
+    color: "Gun Metal"
   },
   {
     id : 'circleSilver' ,
-    src : './images/MAH/MAHCircle3.jpg',
+    src : './images/products/twelveRound/twelveRoundSilver.jpg',
     caption : 'Here is the caption for image 3',
     color: 'Silver'
   },
   {
     id : 'circleGold',
-    src : './images/MAH/MAH12.jpg',
+    src : './images/products/twelveRound/twelveRoundGold.jpg',
     caption : 'Here is the caption for image 4',
     color: 'Gold'
   },
   {
     id : 'circleOrange' ,
-    src : './images/MAH/MAH14.jpg',
+    src : './images/products/twelveRound/twelveRoundOrange.jpg',
     caption : 'Here is the caption for image 3',
     color: 'Orange'
   },
   {
     id : 'circleTeal' ,
-    src : '',
+    src : './images/products/twelveRound/twelveRoundTeal.jpg',
     caption : 'Here is the caption for image 3',
     color: 'Teal'
   },
   {
     id : 'circleNavy' ,
-    src : '',
+    src : './images/products/twelveRound/twelveRoundNavy.jpg',
     caption : 'Here is the caption for image 3',
     color: 'Navy'
   },
@@ -69,43 +69,43 @@ let options = [
 //For square planters
   {
     id : 'squareWhite' ,
-    src : './images/MAH/MAHSquare1.jpeg',
+    src : './images/products/twelveSquare/twelveSquareWhite.jpg',
     caption : 'Here is the caption for image 3',
     color: 'White'
   },
   {
     id : 'squareDarkGray',
-    src : './images/MAH/MAHSquare2.jpeg',
+    src : './images/products/twelveSquare/twelveSquareMetal.jpg',
     caption : 'Here is the caption for desk image',
-    color: 'Dark Gray'
+    color: 'Gun Metal'
   },
   {
     id : 'squareSilver',
-    src : './images/MAH/MAHSquare3.jpeg',
+    src : './images/products/twelveSquare/twelveSquareSilver.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Silver'
   },
   {
     id : 'squareGold',
-    src : './images/MAH/MAHSquare4.jpeg',
+    src : './images/products/twelveSquare/twelveSquareGold.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Gold'
   },
   {
     id : 'squareOrange',
-    src : './images/MAH/MAHSquare5.jpeg',
+    src : './images/products/twelveSquare/twelveSquareOrange.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Orange'
   },
   {
     id : 'squareTeal',
-    src : '',
+    src : './images/products/twelveSquare/twelveSquareTeal.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Teal'
   },
   {
     id : 'squareNavy' ,
-    src : '',
+    src : './images/products/twelveSquare/twelveSquareNavy.jpg',
     caption : 'Here is the caption for image 3',
     color: 'Navy'
   },
@@ -121,7 +121,7 @@ let options = [
     id : 'rectangleDarkGray',
     src : './images/MAH/MAHRectangle2.jpeg',
     caption : 'Here is the caption for desk image',
-    color: 'Dark Gray'
+    color: 'Gun Metal'
   },
   {
     id : 'rectangleSilver',
@@ -157,43 +157,43 @@ let options = [
   //SoftSquare Products
   {
     id : 'softSquareWhite',
-    src : '',
+    src : './images/products/twelveSoft/twelveSoftWhite.jpg',
     caption : 'Here is the caption for desk image',
     color: 'White'
   },
   {
     id : 'softSquareDarkGray',
-    src : '',
+    src : './images/products/twelveSoft/twelveSoftMetal.jpg',
     caption : 'Here is the caption for desk image',
-    color: 'Dark Gray'
+    color: 'Gun Metal'
   },
   {
     id : 'softSquareSilver',
-    src : '',
+    src : './images/products/twelveSoft/twelveSoftSilver.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Silver'
   },
   {
     id : 'softSquareGold',
-    src : '',
+    src : './images/products/twelveSoft/twelveSoftGold.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Gold'
   },
   {
     id : 'softSquareOrange',
-    src : '',
+    src : './images/products/twelveSoft/twelveSoftOrange.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Orange'
   },
   {
     id : 'softSquareTeal',
-    src : '',
+    src : './images/products/twelveSoft/twelveSoftTeal.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Teal'
   },
   {
     id : 'softSquareNavy' ,
-    src : '',
+    src : './images/products/twelveSoft/twelveSoftNavy.jpg',
     caption : 'Here is the caption for image 3',
     color: 'Navy'
   },
@@ -201,43 +201,43 @@ let options = [
   //Triangle Products
   {
     id : 'triangleWhite',
-    src : '',
+    src : './images/products/nineTriangle/nineTriangleWhite.jpg',
     caption : 'Here is the caption for desk image',
     color: 'White'
   },
   {
     id : 'triangleDarkGray',
-    src : '',
+    src : './images/products/nineTriangle/nineTriangleMetal.jpg',
     caption : 'Here is the caption for desk image',
-    color: 'Dark Gray'
+    color: 'Gun Metal'
   },
   {
     id : 'triangleSilver',
-    src : '',
+    src : './images/products/nineTriangle/nineTriangleSilver.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Silver'
   },
   {
     id : 'triangleGold',
-    src : '',
+    src : './images/products/nineTriangle/nineTriangleGold.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Gold'
   },
   {
     id : 'triangleOrange',
-    src : '',
+    src : './images/products/nineTriangle/nineTriangleOrange.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Orange'
   },
   {
     id : 'triangleTeal',
-    src : '',
+    src : './images/products/nineTriangle/nineTriangleTeal.jpg',
     caption : 'Here is the caption for desk image',
     color: 'Teal'
   },
   {
     id : 'triangleNavy' ,
-    src : '',
+    src : './images/products/nineTriangle/nineTriangleNavy.jpg',
     caption : 'Here is the caption for image 3',
     color: 'Navy'
   },
@@ -248,7 +248,8 @@ function imageChange(choice, container) {
   let obj = options.find(o => o.id === choice.id)
   //changes image src to load image, can change text to represent what color it is.
   // $('#' + container.id + '> img').attr('src',obj.src);
-  $('#' + container.id + '>.imageHolder> img').attr('src',obj.src);
+  console.log($('#' + container.id + '>.productLink>.imageHolder> img').attr('src',obj.src));
+  $('#' + container.id + '>.productLink>.imageHolder> img').attr('src',obj.src);
   $('#' + container.id + '>.imageCaption>p>.color').html(obj.color);
   //Adds current color to div, creating border around color.
   $('#' + container.id + '>.options>span.currentColor').removeClass('currentColor');
@@ -263,6 +264,7 @@ function imageChange(choice, container) {
 
   //Either hide img and show noImage div or show img and hide noImage.
   if(obj.src === ''){
+    console.log('src = blank');
     $("#" + container.id).find('.noImage').css('border-color', obj.color.replace(' ', ''));
     // $('#' + container.id + '>.noImage').removeClass('hide');
     // $('#' + container.id + '>img').addClass('hide');
@@ -285,3 +287,29 @@ function imageChange(choice, container) {
   }
 
 };
+$('.choice').on('mouseover', function(){
+  // let color = $(this).attr('class').split(' ')[1];
+  let obj = options.find(o => o.id === this.id)
+  let color = obj.color;
+  let triangle = document.getElementById('triangle');
+  if(triangle !== null){
+    console.log(color);
+    console.log($(this));
+    $(this).html(color);
+  }
+  else{
+    console.log(color);
+    console.log($(this));
+    $(this).html(color);
+  }
+}).on('mouseleave', function(){
+  let triangle = document.getElementById('triangle');
+  if(triangle !== null){
+    console.log(this);
+    $(this).html('');
+  }
+  else{
+    $(this).html('');
+  }
+  // $(this).html('');
+})
